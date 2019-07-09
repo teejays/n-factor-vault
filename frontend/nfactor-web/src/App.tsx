0aff2abc33ffd476cb7e37d737c976cdde8820ac
Talha Ansari <github@teejay.me>
2019-07-09 14:51:24 -0400

    Added babel plugin to allow modular imports for antd

--- frontend/nfactor-web/src/App.tsx
+++ frontend/nfactor-web/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Button from 'antd/es/button';
+import {Button} from 'antd';
 
 const App: React.FC = () => {
   return (
