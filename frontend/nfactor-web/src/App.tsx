1da2648023fd9d9892df9a1359f39b130133ccf2
Talha Ansari <github@teejay.me>
2019-07-17 23:35:57 -0400

    Added a react router and routing to login screen

--- frontend/nfactor-web/src/App.tsx
+++ frontend/nfactor-web/src/App.tsx
@@ -1,56 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
 import './App.css';
-
-import {Typography, Button, Row} from 'antd';
-
 import {PublicLayout} from './layouts/PublicLayout';
-import {LoginForm} from './components/LoginForm';
+import {LoginPage} from './pages/login/LoginPage';
+
 
 const App: React.FC = () => {
+  
   return (
     <div className="App">
-      <PublicLayout 
-      header={"Header"}
-      content={<LoginFormComponent />} 
-      footer={"N-Factor Auth, 2019"}   
-    />
+        <Router>
+        <div className="router">
+          <Route exact path="/" component={withLayout(<LoginPage />)} />
+        </div>
+      </Router>
     </div>
   );
 }
 
-const LoginFormComponent = () => {
-  const { Title } = Typography;
-  return (
-    <Row type="flex" justify="center" align="middle" style={{textAlign: 'center', minHeight: 600}}>
-      <LoginForm 
-        handleLogin={()=>{}}
-        maxWidth={300}
-      />
-    </Row>
-  )
-}
-
 export default App;
 
-const DevWelcomeComponent = () => {
-  return (
-     <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>
-          <Button type="primary">Button</Button>
-        </div>
-    </header>
-  )
-};
+const withLayout = (page: React.ReactNode) => {
+    return(
+      () => <PublicLayout 
+        header={"Header"}
+        content={page} 
+        footer={"N-Factor Auth, 2019"}   
+      />
+    );
+  }
---
+++ frontend/nfactor-web/src/pages/login/LoginPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {Typography, Row} from 'antd';
+
+import {LoginForm} from '../../components/LoginForm';
+
+export const LoginPage = () => {
+    const { Title } = Typography;
+    return (
+        <>
+      <Row type="flex" justify="center" align="middle" style={{textAlign: 'center', minHeight: 600}}>
+        <LoginForm 
+          handleLogin={()=>{}}
+          maxWidth={300}
+        />
+      </Row>
+      </>
+    )
+}
