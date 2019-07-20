a1c4f714889e7a38d5fee0482e28e2525d8dbce7
Talha Ansari <github@teejay.me>
2019-07-20 00:47:14 -0400

    Installed prettier linter config for auto format, and added some comments

--- frontend/nfactor-web/src/App.tsx
+++ frontend/nfactor-web/src/App.tsx
@@ -1,32 +1,37 @@
 import React from 'react';
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import './App.css';
 import {PublicLayout} from './layouts/PublicLayout';
 import {LoginPage} from './pages/login/LoginPage';
 
-
 const App: React.FC = () => {
-  
   return (
+    // This is the entry point for the app.
+    // Let's only put the routes here, and then put the pages
+    // in the pages directory, and components shareable among pages
+    // in the components directory
     <div className="App">
-        <Router>
+      <Router>
         <div className="router">
           <Route exact path="/" component={withLayout(<LoginPage />)} />
         </div>
       </Router>
     </div>
   );
-}
+};
 
 export default App;
 
+// withLayout is a higher order component (HOC), it basically
+// wraps around a component (page) and put's it in a 'layout',
+// with  a standard header and footer.
 const withLayout = (page: React.ReactNode) => {
-    return(
-      () => <PublicLayout 
-        header={"Header"}
-        content={page} 
-        footer={"N-Factor Auth, 2019"}   
-      />
-    );
-  }
+  return () => (
+    <PublicLayout
+      header={'Header'}
+      content={page}
+      footer={'N-Factor Auth, 2019'}
+    />
+  );
+};
--- frontend/nfactor-web/src/pages/login/LoginPage.tsx
+++ frontend/nfactor-web/src/pages/login/LoginPage.tsx
@@ -5,15 +5,16 @@ import {Typography, Row} from 'antd';
 import {LoginForm} from '../../components/LoginForm';
 
 export const LoginPage = () => {
-    const { Title } = Typography;
-    return (
-        <>
-      <Row type="flex" justify="center" align="middle" style={{textAlign: 'center', minHeight: 600}}>
-        <LoginForm 
-          handleLogin={()=>{}}
-          maxWidth={300}
-        />
+  const {Title} = Typography;
+  return (
+    <>
+      <Row
+        type="flex"
+        justify="center"
+        align="middle"
+        style={{textAlign: 'center', minHeight: 600}}>
+        <LoginForm handleLogin={() => {}} maxWidth={300} />
       </Row>
-      </>
-    )
-}
+    </>
+  );
+};
